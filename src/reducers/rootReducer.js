e616ff6294c1ec400a52383c227a0c6732c00e26
sean-slaughter <sthompson2822@gmail.com>
2020-10-14 12:04:02 -0500

    Add initial state for reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,27 @@
-const rootReducer = (state = {}, action) =>{
+//get dimensions for grid based on window size
+const width = Math.floor(window.innerWidth * 0.5);
+const height = Math.floor(window.innerHeight * 0.7);
+const rows = Math.floor(Math.floor(window.innerHeight * 0.7)/10);
+const cols = Math.floor(Math.floor(window.innerWidth * 0.5)/10);
+const gameGrid = Array(rows).fill().map(() => Array(cols).fill(false));
+
+//declare initial state for reducer
+const initialState = {
+    dim:{
+        width: width,
+        height: height,
+        rows: rows,
+        cols: cols
+    },
+    gameGrid: gameGrid
+
+}
+
+const rootReducer = (state = initialState, action) =>{
     switch(action.type){
         default:
             return state
     }
 }
+
+export default rootReducer
