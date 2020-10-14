f37eb6fd30c1d0cc8a26f55f502c7916f090d314
sean-slaughter <sthompson2822@gmail.com>
2020-10-14 12:36:42 -0500

    add drawcanvas method to canvas component

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -19,5 +19,25 @@ export class Canvas extends Component {
             <canvas ref={this.canvas} width={this.width} height={this.height}></canvas>
         )
     }
+
+    componentDidMount(){
+        this.drawCanvas()
+    }
+
+    drawCanvas = () => {
+        const canvas = this.canvas.current;
+        if(canvas){
+            const ctx = canvas.getContext('2d');
+            ctx.strokeStyle = "black";
+            for(let i = 0; i < this.cols; i++){
+                for(let j = 0; j < this.rows; j++){
+                    ctx.fillStyle = "white";
+                    ctx.strokeRect(i * this.cellSize, j * this.cellSize, this.cellSize, this.cellSize)
+                    ctx.fillRect(i * this.cellSize, j * this.cellSize, this.cellSize, this.cellSize)
+                }
+            }
+        }
+
+    }
 }
 export default Canvas
