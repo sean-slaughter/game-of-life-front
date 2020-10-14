25ed1e8a281c82b602a6b512c4dfcebd4f584fef
sean-slaughter <sthompson2822@gmail.com>
2020-10-14 12:25:00 -0500

    Wrap game canvas in material ui grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import CanvasContainer from './containers/CanvasContainer'
+import Grid from '@material-ui/core/Grid';
+
 
 function App() {
   return (
     <div className="App">
+      <Grid container justify="center" alignItems="center">
         <CanvasContainer/>
+      </Grid>
     </div>
   );
 }
