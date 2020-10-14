6f90871584fc564de2b0f918a5ce16b59edccd01
sean-slaughter <sthompson2822@gmail.com>
2020-10-13 22:15:15 -0500

    Add component/container/reducer/action classes

---
+++ src/containers/CanvasContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CanvasContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CanvasContainer
